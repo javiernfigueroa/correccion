90696379816708834ad0e4601224429fd5b65942
Javier Figueroa <javiernfigueroa@gmail.com>
2024-12-19 23:01:21 -0300

    revisar

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,66 +1,64 @@
 import React, { useContext } from "react";
-import { FaPizzaSlice, FaCartArrowDown  } from "react-icons/fa6";
+import { FaPizzaSlice, FaCartArrowDown } from "react-icons/fa6";
 import { MdReadMore } from "react-icons/md";
-import {formatNumber} from './funcionesJs.js'
+import { formatNumber } from './funcionesJs.js'
 import { CartContext } from "../context/CartContext.jsx";
 import { useNavigate } from "react-router-dom";
 
-const CardPizza = ({ pizza }) => {  
-  const {cart, setCart} = useContext(CartContext);
+const CardPizza = ({ pizza }) => {
+  const { cart, setCart } = useContext(CartContext);
   const priceFormat = formatNumber(pizza.price);
   const navigate = useNavigate();
 
-  const viewMore=()=>{
+  const viewMore = () => {
     navigate(`/pizza/${pizza.id}`);
   }
 
-  const addToCart=()=>{
-    /* const pi= cart.filter((p)=>p.id === pizza.id);
-    const key= cart.findIndex((p)=>p.id === pizza.id); */
-    console.log(pizza.id)
-    console.log(cart);
-    const existingPizza= cart.find((p)=>p.id===pizza.id);
-    console.log(existingPizza);
-    if(!existingPizza){
-      const newPizza= {
-        id: pizza.id, 
-        name: pizza.name, 
-        price: pizza.price, 
-        count: 1, 
-        img: pizza.img,
-      };  
-      setCart([...cart, newPizza]);
-      alert(`Pizza ${pizza.name.toUpperCase()} agregada al carrito `);
-    }else{
-      /* cart[key].count++;
-      setCart([...cart])  */ 
-      const updateCart = cart.map((p)=>
-        p.id === pizza.id ? {...p, count: p.count + 1} : p
+  const addToCart = () => {
+    const existingPizza = cart.find((p) => p.id.toLowerCase() === pizza.id.toLowerCase());
+
+    if (existingPizza) {
+
+      const updatedCart = cart.map((p) =>
+        p.id.toLowerCase() === pizza.id.toLowerCase()
+          ? { ...p, count: p.count + 1 }
+          : p
       );
-      setCart(updateCart);
-      alert(`Pizza ${pizza.name.toUpperCase()} agregada al carrito `);
+      setCart(updatedCart);
+      alert(`Pizza ${pizza.name.toUpperCase()} agregada al carrito nuevamente.`);
+    } else {
+
+      const newPizza = {
+        id: pizza.id,
+        name: pizza.name,
+        price: pizza.price,
+        count: 1,
+        img: pizza.img,
+      };
+      setCart([...cart, newPizza]);
+      alert(`Pizza ${pizza.name.toUpperCase()} agregada al carrito.`);
     }
-    console.log(cart);
-  }
+  };
+
   return (
-    <div style={{margin:"0.5em 0.5em 0.5em 0.5em"}} className="max-w-md rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={pizza.img} alt={pizza.name}/>
+    <div style={{ margin: "0.5em 0.5em 0.5em 0.5em" }} className="max-w-md rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={pizza.img} alt={pizza.name} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{pizza.name}</div>
-        <p style={{display:"flex"}} className="text-gray-700 text-base"><FaPizzaSlice/><span> Ingredientes:</span></p>
+        <p style={{ display: "flex" }} className="text-gray-700 text-base"><FaPizzaSlice /><span> Ingredientes:</span></p>
         <ul className="list-disc">
           {pizza.ingredients.join(", ")}
         </ul>
         <p className="text-gray-700 text-base">Precio: ${priceFormat}.-</p>
       </div>
       <div className="px-6 pt-4 pb-2">
-      <button className="inline-flex bg-red-200 rounded-full px-5 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        onClick={viewMore}>
-          <span>Ver mas</span> <MdReadMore/>
+        <button className="inline-flex bg-red-200 rounded-full px-5 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          onClick={viewMore}>
+          <span>Ver mas</span> <MdReadMore />
         </button>
         <button className="inline-flex bg-green-100 rounded-full px-5 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 "
-        onClick={(addToCart)}>
-          <span>Agregar</span> <FaCartArrowDown/>
+          onClick={(addToCart)}>
+          <span>Agregar</span> <FaCartArrowDown />
         </button>
       </div>
     </div>
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,106 +1,109 @@
-import React, { useContext, useState } from "react";
-import { pizzas } from "../data/pizzas.js";
+import React, { useContext } from "react";
 import { formatNumber } from "./funcionesJs.js";
 import { FaPlus, FaMinus, FaTrashAlt } from "react-icons/fa";
 import { CartContext } from "../context/CartContext.jsx";
 
 const Cart = () => {
-    const {cart, setCart} = useContext(CartContext);
-    const [datoPizza, setDatoPizza] = useState(pizzas);
+    const { cart, setCart } = useContext(CartContext);
+
     const sumarPizza = (key) => {
-        cart[key].count++;
-        setCart([...cart]);
+        const updatedCart = cart.map((pizza, index) =>
+            index === key ? { ...pizza, count: pizza.count + 1 } : pizza
+        );
+        setCart(updatedCart);
     };
 
     const restarPizza = (key) => {
-        if(cart[key].count===1){
-            let text=`La pizza ${cart[key].name.toLocaleUpperCase()},\n sera eliminada del carrito`
-            if (confirm(text) == true) {
-                cart[key].count--;
-                setCart([...cart.filter((pizza) => pizza.count > 0)]);
-            } else {
-                setCart([...cart])
-            }
-        }else{
-            cart[key].count--;
-            setCart([...cart])
-        }       
+        const updatedCart = cart
+            .map((pizza, index) =>
+                index === key
+                    ? { ...pizza, count: pizza.count > 1 ? pizza.count - 1 : 0 }
+                    : pizza
+            )
+            .filter((pizza) => pizza.count > 0);
+
+        setCart(updatedCart);
     };
 
-    const calculaTotal=()=>cart.reduce((total, pizza) => total + pizza.price * pizza.count,0);
-    
+    const calculaTotal = () =>
+        cart.reduce((total, pizza) => total + pizza.price * pizza.count, 0);
+
     return (
-        <>
-        <div className="grid justify-items-center border-solid border-4" >
+        <div className="grid justify-items-center border-solid border-4">
             <div className="w-2/4 px-4 py-6 sm:px-6">
-            <h2
-                className="text-lg font-medium text-center bg-orange-300 text-gray-900"
-            >
-                Carrito de compras
-            </h2>
-            <ul role="list" className="divide-y divide-gray-200">
-                {cart.map((pizza, key) => {
-                const eleccion = datoPizza.filter((p) => p.id === pizza.id);
-                return (
-                    <li className="flex" key={key}>
-                    <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
-                        <img
-                        src={pizza.img}
-                        alt={eleccion[0].desc}
-                        className="size-full object-cover"
-                        />
-                    </div>
-                    <div className="ml-4 flex flex-1 flex-col">
-                        <div>
-                        <div className="flex justify-between text-base font-medium text-gray-900">
-                            <h3>{pizza.name.toUpperCase()}</h3>
-                            <p className="ml-4">${formatNumber(pizza.price)}.-</p>
-                        </div>
-                        <p className="mt-1 text-sm text-gray-500">
-                            {eleccion[0].ingredients.join(", ")}
-                        </p>
-                        </div>
-                        <div className="flex flex-1 items-end justify-between text-lg">
-                        <p className="text-gray-500">Cantidad {pizza.count}</p>                        
-                            <button
-                            type="button"
-                            className="font-medium text-indigo-600 hover:text-indigo-500"
-                            onClick={() => sumarPizza(key)}
-                            >
-                            <FaPlus />
-                            </button>                       
-                            <button
-                            type="button"
-                            className="font-lg text-indigo-600 hover:text-indigo-500"
-                            onClick={() => restarPizza(key)}
-                            >
-                            {(pizza.count!==1)?<FaMinus />: <FaTrashAlt/> }
-                            </button>                       
-                        </div>
-                    </div>
-                    </li>
-                );
-                })}
-            </ul>
+                <h2 className="text-lg font-medium text-center bg-orange-300 text-gray-900">
+                    Carrito de compras
+                </h2>
+                {cart.length === 0 ? (
+                    <p className="text-center text-gray-500">
+                        El carrito está vacío.
+                    </p>
+                ) : (
+                    <ul role="list" className="divide-y divide-gray-200">
+                        {cart.map((pizza, key) => (
+                            <li className="flex" key={key}>
+                                <div className="size-24 shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                    <img
+                                        src={pizza.img}
+                                        alt={pizza.name}
+                                        className="size-full object-cover"
+                                    />
+                                </div>
+                                <div className="ml-4 flex flex-1 flex-col">
+                                    <div>
+                                        <div className="flex justify-between text-base font-medium text-gray-900">
+                                            <h3>{pizza.name.toUpperCase()}</h3>
+                                            <p className="ml-4">
+                                                ${formatNumber(pizza.price)}.-
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-1 items-end justify-between text-lg">
+                                        <p className="text-gray-500">
+                                            Cantidad {pizza.count}
+                                        </p>
+                                        <button
+                                            type="button"
+                                            className="font-medium text-indigo-600 hover:text-indigo-500"
+                                            onClick={() => sumarPizza(key)}
+                                        >
+                                            <FaPlus />
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="font-lg text-indigo-600 hover:text-indigo-500"
+                                            onClick={() => restarPizza(key)}
+                                        >
+                                            {pizza.count !== 1 ? (
+                                                <FaMinus />
+                                            ) : (
+                                                <FaTrashAlt />
+                                            )}
+                                        </button>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
             <div className="border-t border-gray-200 w-2/4 px-4 py-6 sm:px-6">
-            <div className="flex justify-between text-base font-medium text-gray-900">
-                <p>Subtotal</p>
-                <p>${formatNumber(calculaTotal())}.-</p>
-            </div>
-            <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
-                <button
-                type="button"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                Pagar
-                <span aria-hidden="true"> &rarr;</span>
-                </button>
-            </div>
+                <div className="flex justify-between text-base font-medium text-gray-900">
+                    <p>Subtotal</p>
+                    <p>${formatNumber(calculaTotal())}.-</p>
+                </div>
+                <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+                    <button
+                        type="button"
+                        className="font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                        Pagar
+                        <span aria-hidden="true"> &rarr;</span>
+                    </button>
+                </div>
             </div>
         </div>
-        </>
     );
-    };
+};
 
-    export default Cart;
+export default Cart;
